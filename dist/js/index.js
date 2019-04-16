0beb0200b66b9dc048ab3322620c0d759800d8be
asahi <asahi@qq.com>
2019-04-16 16:00:10 +0800

    'binary'

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
     whenResize();
 
     function whenResize() {
-fillDiv('v', $(window), $('.r2'), $('.r1'), $('.r3'));
-fillDiv('h', $('.r2'), $('.r2-c2'), $('.r2-c1'));
-fillDiv('h', $('.r2-c2'), $('.r2-c2-c2'), $('.r2-c2-c1'), $('.r2-c2-c3'));
 }
 
 $(window).resize(function () {
